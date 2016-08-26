b0f2eac419e112e79bda680cd2b45b9d012c0124
spillmoon <spillmoon@naver.com>
2016-08-26 13:34:41 +0900

    dev_service, 스키마 생성 추가 / 공연, 공연장 더미 데이터 삽입 쿼리문 일부 추가

---
+++ query/insert_data_to_play_table.sql
@@ -0,0 +1,109 @@
+INSERT INTO play (name, place_id, playDay, playTime, theme, VIPprice, Rprice, Sprice, salePer, starScoreAvg)
+VALUES ('잭더리퍼', 1, str_to_date('2016-09-01', '%Y-%m-%d'), date_format('2016-09-01 20:00', '%H:%i'), '뮤지컬', 130000, 110000, 110000, 0.55, 9.2);
+INSERT INTO play (name, place_id, playDay, playTime, theme, VIPprice, Rprice, Sprice, salePer, starScoreAvg)
+VALUES ('잭더리퍼', 1, str_to_date('2016-09-02', '%Y-%m-%d'), date_format('2016-09-02 20:00', '%H:%i'), '뮤지컬', 130000, 110000, 110000, 0.55, 9.2);
+INSERT INTO play (name, place_id, playDay, playTime, theme, VIPprice, Rprice, Sprice, salePer, starScoreAvg)
+VALUES ('잭더리퍼', 1, str_to_date('2016-09-03', '%Y-%m-%d'), date_format('2016-09-03 15:00', '%H:%i'), '뮤지컬', 130000, 110000, 110000, 0.55, 9.2);
+INSERT INTO play (name, place_id, playDay, playTime, theme, VIPprice, Rprice, Sprice, salePer, starScoreAvg)
+VALUES ('잭더리퍼', 1, str_to_date('2016-09-03', '%Y-%m-%d'), date_format('2016-09-03 19:00', '%H:%i'), '뮤지컬', 130000, 110000, 110000, 0.55, 9.2);
+INSERT INTO play (name, place_id, playDay, playTime, theme, VIPprice, Rprice, Sprice, salePer, starScoreAvg)
+VALUES ('잭더리퍼', 1, str_to_date('2016-09-04', '%Y-%m-%d'), date_format('2016-09-04 15:00', '%H:%i'), '뮤지컬', 130000, 110000, 110000, 0.55, 9.2);
+INSERT INTO play (name, place_id, playDay, playTime, theme, VIPprice, Rprice, Sprice, salePer, starScoreAvg)
+VALUES ('잭더리퍼', 1, str_to_date('2016-09-06', '%Y-%m-%d'), date_format('2016-09-06 20:00', '%H:%i'), '뮤지컬', 130000, 110000, 110000, 0.55, 9.2);
+INSERT INTO play (name, place_id, playDay, playTime, theme, VIPprice, Rprice, Sprice, salePer, starScoreAvg)
+VALUES ('잭더리퍼', 1, str_to_date('2016-09-07', '%Y-%m-%d'), date_format('2016-09-07 20:00', '%H:%i'), '뮤지컬', 130000, 110000, 110000, 0.55, 9.2);
+INSERT INTO play (name, place_id, playDay, playTime, theme, VIPprice, Rprice, Sprice, salePer, starScoreAvg)
+VALUES ('잭더리퍼', 1, str_to_date('2016-09-08', '%Y-%m-%d'), date_format('2016-09-08 20:00', '%H:%i'), '뮤지컬', 130000, 110000, 110000, 0.55, 9.2);
+INSERT INTO play (name, place_id, playDay, playTime, theme, VIPprice, Rprice, Sprice, salePer, starScoreAvg)
+VALUES ('잭더리퍼', 1, str_to_date('2016-09-09', '%Y-%m-%d'), date_format('2016-09-09 20:00', '%H:%i'), '뮤지컬', 130000, 110000, 110000, 0.55, 9.2);
+INSERT INTO play (name, place_id, playDay, playTime, theme, VIPprice, Rprice, Sprice, salePer, starScoreAvg)
+VALUES ('잭더리퍼', 1, str_to_date('2016-09-10', '%Y-%m-%d'), date_format('2016-09-10 15:00', '%H:%i'), '뮤지컬', 130000, 110000, 110000, 0.55, 9.2);
+INSERT INTO play (name, place_id, playDay, playTime, theme, VIPprice, Rprice, Sprice, salePer, starScoreAvg)
+VALUES ('잭더리퍼', 1, str_to_date('2016-09-10', '%Y-%m-%d'), date_format('2016-09-10 19:00', '%H:%i'), '뮤지컬', 130000, 110000, 110000, 0.55, 9.2);
+INSERT INTO play (name, place_id, playDay, playTime, theme, VIPprice, Rprice, Sprice, salePer, starScoreAvg)
+VALUES ('잭더리퍼', 1, str_to_date('2016-09-11', '%Y-%m-%d'), date_format('2016-09-11 20:00', '%H:%i'), '뮤지컬', 130000, 110000, 110000, 0.55, 9.2);
+INSERT INTO play (name, place_id, playDay, playTime, theme, VIPprice, Rprice, Sprice, salePer, starScoreAvg)
+VALUES ('잭더리퍼', 1, str_to_date('2016-09-13', '%Y-%m-%d'), date_format('2016-09-13 20:00', '%H:%i'), '뮤지컬', 130000, 110000, 110000, 0.55, 9.2);
+INSERT INTO play (name, place_id, playDay, playTime, theme, VIPprice, Rprice, Sprice, salePer, starScoreAvg)
+VALUES ('잭더리퍼', 1, str_to_date('2016-09-14', '%Y-%m-%d'), date_format('2016-09-14 15:00', '%H:%i'), '뮤지컬', 130000, 110000, 110000, 0.55, 9.2);
+INSERT INTO play (name, place_id, playDay, playTime, theme, VIPprice, Rprice, Sprice, salePer, starScoreAvg)
+VALUES ('잭더리퍼', 1, str_to_date('2016-09-14', '%Y-%m-%d'), date_format('2016-09-14 19:00', '%H:%i'), '뮤지컬', 130000, 110000, 110000, 0.55, 9.2);
+INSERT INTO play (name, place_id, playDay, playTime, theme, VIPprice, Rprice, Sprice, salePer, starScoreAvg)
+VALUES ('잭더리퍼', 1, str_to_date('2016-09-16', '%Y-%m-%d'), date_format('2016-09-16 15:00', '%H:%i'), '뮤지컬', 130000, 110000, 110000, 0.55, 9.2);
+INSERT INTO play (name, place_id, playDay, playTime, theme, VIPprice, Rprice, Sprice, salePer, starScoreAvg)
+VALUES ('잭더리퍼', 1, str_to_date('2016-09-16', '%Y-%m-%d'), date_format('2016-09-16 19:00', '%H:%i'), '뮤지컬', 130000, 110000, 110000, 0.55, 9.2);
+INSERT INTO play (name, place_id, playDay, playTime, theme, VIPprice, Rprice, Sprice, salePer, starScoreAvg)
+VALUES ('잭더리퍼', 1, str_to_date('2016-09-17', '%Y-%m-%d'), date_format('2016-09-17 15:00', '%H:%i'), '뮤지컬', 130000, 110000, 110000, 0.55, 9.2);
+INSERT INTO play (name, place_id, playDay, playTime, theme, VIPprice, Rprice, Sprice, salePer, starScoreAvg)
+VALUES ('잭더리퍼', 1, str_to_date('2016-09-17', '%Y-%m-%d'), date_format('2016-09-17 19:00', '%H:%i'), '뮤지컬', 130000, 110000, 110000, 0.55, 9.2);
+INSERT INTO play (name, place_id, playDay, playTime, theme, VIPprice, Rprice, Sprice, salePer, starScoreAvg)
+VALUES ('잭더리퍼', 1, str_to_date('2016-09-18', '%Y-%m-%d'), date_format('2016-09-18 15:00', '%H:%i'), '뮤지컬', 130000, 110000, 110000, 0.55, 9.2);
+INSERT INTO play (name, place_id, playDay, playTime, theme, VIPprice, Rprice, Sprice, salePer, starScoreAvg)
+VALUES ('잭더리퍼', 1, str_to_date('2016-09-20', '%Y-%m-%d'), date_format('2016-09-20 20:00', '%H:%i'), '뮤지컬', 130000, 110000, 110000, 0.55, 9.2);
+INSERT INTO play (name, place_id, playDay, playTime, theme, VIPprice, Rprice, Sprice, salePer, starScoreAvg)
+VALUES ('잭더리퍼', 1, str_to_date('2016-09-21', '%Y-%m-%d'), date_format('2016-09-21 20:00', '%H:%i'), '뮤지컬', 130000, 110000, 110000, 0.55, 9.2);
+INSERT INTO play (name, place_id, playDay, playTime, theme, VIPprice, Rprice, Sprice, salePer, starScoreAvg)
+VALUES ('잭더리퍼', 1, str_to_date('2016-09-22', '%Y-%m-%d'), date_format('2016-09-22 20:00', '%H:%i'), '뮤지컬', 130000, 110000, 110000, 0.55, 9.2);
+INSERT INTO play (name, place_id, playDay, playTime, theme, VIPprice, Rprice, Sprice, salePer, starScoreAvg)
+VALUES ('잭더리퍼', 1, str_to_date('2016-09-23', '%Y-%m-%d'), date_format('2016-09-23 20:00', '%H:%i'), '뮤지컬', 130000, 110000, 110000, 0.55, 9.2);
+INSERT INTO play (name, place_id, playDay, playTime, theme, VIPprice, Rprice, Sprice, salePer, starScoreAvg)
+VALUES ('잭더리퍼', 1, str_to_date('2016-09-24', '%Y-%m-%d'), date_format('2016-09-24 15:00', '%H:%i'), '뮤지컬', 130000, 110000, 110000, 0.55, 9.2);
+INSERT INTO play (name, place_id, playDay, playTime, theme, VIPprice, Rprice, Sprice, salePer, starScoreAvg)
+VALUES ('잭더리퍼', 1, str_to_date('2016-09-24', '%Y-%m-%d'), date_format('2016-09-24 19:00', '%H:%i'), '뮤지컬', 130000, 110000, 110000, 0.55, 9.2);
+INSERT INTO play (name, place_id, playDay, playTime, theme, VIPprice, Rprice, Sprice, salePer, starScoreAvg)
+VALUES ('잭더리퍼', 1, str_to_date('2016-09-25', '%Y-%m-%d'), date_format('2016-09-25 15:00', '%H:%i'), '뮤지컬', 130000, 110000, 110000, 0.55, 9.2);
+INSERT INTO play (name, place_id, playDay, playTime, theme, VIPprice, Rprice, Sprice, salePer, starScoreAvg)
+VALUES ('키다리아저씨', 2, str_to_date('2016-09-01', '%Y-%m-%d'), date_format('2016-09-01 20:00', '%H:%i'), '뮤지컬', 66000, 66000, 66000, 0.6, 8.8);
+INSERT INTO play (name, place_id, playDay, playTime, theme, VIPprice, Rprice, Sprice, salePer, starScoreAvg)
+VALUES ('키다리아저씨', 2, str_to_date('2016-09-02', '%Y-%m-%d'), date_format('2016-09-02 20:00', '%H:%i'), '뮤지컬', 66000, 66000, 66000, 0.6, 8.8);
+INSERT INTO play (name, place_id, playDay, playTime, theme, VIPprice, Rprice, Sprice, salePer, starScoreAvg)
+VALUES ('키다리아저씨', 2, str_to_date('2016-09-03', '%Y-%m-%d'), date_format('2016-09-03 15:00', '%H:%i'), '뮤지컬', 66000, 66000, 66000, 0.6, 8.8);
+INSERT INTO play (name, place_id, playDay, playTime, theme, VIPprice, Rprice, Sprice, salePer, starScoreAvg)
+VALUES ('키다리아저씨', 2, str_to_date('2016-09-03', '%Y-%m-%d'), date_format('2016-09-03 19:00', '%H:%i'), '뮤지컬', 66000, 66000, 66000, 0.6, 8.8);
+INSERT INTO play (name, place_id, playDay, playTime, theme, VIPprice, Rprice, Sprice, salePer, starScoreAvg)
+VALUES ('키다리아저씨', 2, str_to_date('2016-09-04', '%Y-%m-%d'), date_format('2016-09-04 15:00', '%H:%i'), '뮤지컬', 66000, 66000, 66000, 0.6, 8.8);
+INSERT INTO play (name, place_id, playDay, playTime, theme, VIPprice, Rprice, Sprice, salePer, starScoreAvg)
+VALUES ('키다리아저씨', 2, str_to_date('2016-09-04', '%Y-%m-%d'), date_format('2016-09-04 19:00', '%H:%i'), '뮤지컬', 66000, 66000, 66000, 0.6, 8.8);
+INSERT INTO play (name, place_id, playDay, playTime, theme, VIPprice, Rprice, Sprice, salePer, starScoreAvg)
+VALUES ('키다리아저씨', 2, str_to_date('2016-09-06', '%Y-%m-%d'), date_format('2016-09-06 20:00', '%H:%i'), '뮤지컬', 66000, 66000, 66000, 0.6, 8.8);
+INSERT INTO play (name, place_id, playDay, playTime, theme, VIPprice, Rprice, Sprice, salePer, starScoreAvg)
+VALUES ('키다리아저씨', 2, str_to_date('2016-09-07', '%Y-%m-%d'), date_format('2016-09-07 20:00', '%H:%i'), '뮤지컬', 66000, 66000, 66000, 0.6, 8.8);
+INSERT INTO play (name, place_id, playDay, playTime, theme, VIPprice, Rprice, Sprice, salePer, starScoreAvg)
+VALUES ('키다리아저씨', 2, str_to_date('2016-09-08', '%Y-%m-%d'), date_format('2016-09-08 20:00', '%H:%i'), '뮤지컬', 66000, 66000, 66000, 0.6, 8.8);
+INSERT INTO play (name, place_id, playDay, playTime, theme, VIPprice, Rprice, Sprice, salePer, starScoreAvg)
+VALUES ('키다리아저씨', 2, str_to_date('2016-09-09', '%Y-%m-%d'), date_format('2016-09-09 20:00', '%H:%i'), '뮤지컬', 66000, 66000, 66000, 0.6, 8.8);
+INSERT INTO play (name, place_id, playDay, playTime, theme, VIPprice, Rprice, Sprice, salePer, starScoreAvg)
+VALUES ('키다리아저씨', 2, str_to_date('2016-09-10', '%Y-%m-%d'), date_format('2016-09-10 15:00', '%H:%i'), '뮤지컬', 66000, 66000, 66000, 0.6, 8.8);
+INSERT INTO play (name, place_id, playDay, playTime, theme, VIPprice, Rprice, Sprice, salePer, starScoreAvg)
+VALUES ('키다리아저씨', 2, str_to_date('2016-09-10', '%Y-%m-%d'), date_format('2016-09-10 19:00', '%H:%i'), '뮤지컬', 66000, 66000, 66000, 0.6, 8.8);
+INSERT INTO play (name, place_id, playDay, playTime, theme, VIPprice, Rprice, Sprice, salePer, starScoreAvg)
+VALUES ('키다리아저씨', 2, str_to_date('2016-09-11', '%Y-%m-%d'), date_format('2016-09-11 15:00', '%H:%i'), '뮤지컬', 66000, 66000, 66000, 0.6, 8.8);
+INSERT INTO play (name, place_id, playDay, playTime, theme, VIPprice, Rprice, Sprice, salePer, starScoreAvg)
+VALUES ('키다리아저씨', 2, str_to_date('2016-09-11', '%Y-%m-%d'), date_format('2016-09-11 19:00', '%H:%i'), '뮤지컬', 66000, 66000, 66000, 0.6, 8.8);
+INSERT INTO play (name, place_id, playDay, playTime, theme, VIPprice, Rprice, Sprice, salePer, starScoreAvg)
+VALUES ('키다리아저씨', 2, str_to_date('2016-09-13', '%Y-%m-%d'), date_format('2016-09-13 20:00', '%H:%i'), '뮤지컬', 66000, 66000, 66000, 0.6, 8.8);
+INSERT INTO play (name, place_id, playDay, playTime, theme, VIPprice, Rprice, Sprice, salePer, starScoreAvg)
+VALUES ('키다리아저씨', 2, str_to_date('2016-09-17', '%Y-%m-%d'), date_format('2016-09-17 15:00', '%H:%i'), '뮤지컬', 66000, 66000, 66000, 0.6, 8.8);
+INSERT INTO play (name, place_id, playDay, playTime, theme, VIPprice, Rprice, Sprice, salePer, starScoreAvg)
+VALUES ('키다리아저씨', 2, str_to_date('2016-09-17', '%Y-%m-%d'), date_format('2016-09-17 19:00', '%H:%i'), '뮤지컬', 66000, 66000, 66000, 0.6, 8.8);
+INSERT INTO play (name, place_id, playDay, playTime, theme, VIPprice, Rprice, Sprice, salePer, starScoreAvg)
+VALUES ('키다리아저씨', 2, str_to_date('2016-09-18', '%Y-%m-%d'), date_format('2016-09-18 15:00', '%H:%i'), '뮤지컬', 66000, 66000, 66000, 0.6, 8.8);
+INSERT INTO play (name, place_id, playDay, playTime, theme, VIPprice, Rprice, Sprice, salePer, starScoreAvg)
+VALUES ('키다리아저씨', 2, str_to_date('2016-09-18', '%Y-%m-%d'), date_format('2016-09-18 19:00', '%H:%i'), '뮤지컬', 66000, 66000, 66000, 0.6, 8.8);
+INSERT INTO play (name, place_id, playDay, playTime, theme, VIPprice, Rprice, Sprice, salePer, starScoreAvg)
+VALUES ('키다리아저씨', 2, str_to_date('2016-09-20', '%Y-%m-%d'), date_format('2016-09-20 20:00', '%H:%i'), '뮤지컬', 66000, 66000, 66000, 0.6, 8.8);
+INSERT INTO play (name, place_id, playDay, playTime, theme, VIPprice, Rprice, Sprice, salePer, starScoreAvg)
+VALUES ('키다리아저씨', 2, str_to_date('2016-09-21', '%Y-%m-%d'), date_format('2016-09-21 20:00', '%H:%i'), '뮤지컬', 66000, 66000, 66000, 0.6, 8.8);
+INSERT INTO play (name, place_id, playDay, playTime, theme, VIPprice, Rprice, Sprice, salePer, starScoreAvg)
+VALUES ('키다리아저씨', 2, str_to_date('2016-09-22', '%Y-%m-%d'), date_format('2016-09-22 20:00', '%H:%i'), '뮤지컬', 66000, 66000, 66000, 0.6, 8.8);
+INSERT INTO play (name, place_id, playDay, playTime, theme, VIPprice, Rprice, Sprice, salePer, starScoreAvg)
+VALUES ('키다리아저씨', 2, str_to_date('2016-09-23', '%Y-%m-%d'), date_format('2016-09-23 20:00', '%H:%i'), '뮤지컬', 66000, 66000, 66000, 0.6, 8.8);
+INSERT INTO play (name, place_id, playDay, playTime, theme, VIPprice, Rprice, Sprice, salePer, starScoreAvg)
+VALUES ('키다리아저씨', 2, str_to_date('2016-09-24', '%Y-%m-%d'), date_format('2016-09-24 15:00', '%H:%i'), '뮤지컬', 66000, 66000, 66000, 0.6, 8.8);
+INSERT INTO play (name, place_id, playDay, playTime, theme, VIPprice, Rprice, Sprice, salePer, starScoreAvg)
+VALUES ('키다리아저씨', 2, str_to_date('2016-09-24', '%Y-%m-%d'), date_format('2016-09-24 19:00', '%H:%i'), '뮤지컬', 66000, 66000, 66000, 0.6, 8.8);
+INSERT INTO play (name, place_id, playDay, playTime, theme, VIPprice, Rprice, Sprice, salePer, starScoreAvg)
+VALUES ('키다리아저씨', 2, str_to_date('2016-09-25', '%Y-%m-%d'), date_format('2016-09-25 15:00', '%H:%i'), '뮤지컬', 66000, 66000, 66000, 0.6, 8.8);
+INSERT INTO play (name, place_id, playDay, playTime, theme, VIPprice, Rprice, Sprice, salePer, starScoreAvg)
+VALUES ('키다리아저씨', 2, str_to_date('2016-09-25', '%Y-%m-%d'), date_format('2016-09-25 19:00', '%H:%i'), '뮤지컬', 66000, 66000, 66000, 0.6, 8.8);
+
